0c9a59e15154f7ce67028ab3eb358fc5b321ab72
Alessandro Cappellini <cappe9275@hotmail.it>
2017-04-26 17:26:25 +0200

    post in flask_restful OK and route get in flask OKù

--- serverblogpost.py
+++ serverblogpost.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 import sqlite3
 from flask import Flask, render_template, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
 conn = sqlite3.connect('blog.db', check_same_thread=False)
 conn.row_factory = sqlite3.Row
 c = conn.cursor()
 
-
-
-@app.route('/', methods=['GET','POST'])
-def index():
-    if request.method == 'GET':
-        sql = 'SELECT * FROM posts'
-        ctx = {'posts': c.execute(sql)}
-        return render_template('index.html', **ctx)
-
-    elif request.method == 'POST':
+class PostBlog(Resource):
+    def post(self):
         params = [
             request.form['title'],
             request.form['data'],
@@ -24,9 +19,19 @@ def index():
         ]
         sql = ''' insert into posts(title, data, post)
                 values(?,?,?)'''
+
         c.execute(sql,params)
         conn.commit()
-        return '',201
+        return '', 201
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    sql = 'SELECT * FROM posts'
+    ctx = {'posts': c.execute(sql)}
+
+    return render_template('index.html', **ctx)
+
+api.add_resource(PostBlog, '/posts')
 
 if __name__ == '__main__':
     app.run(debug=True)
