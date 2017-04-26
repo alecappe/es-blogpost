0154671b8bef3d1e09b7c3022dfb9fca06b0aedc
Alessandro Cappellini <cappe9275@hotmail.it>
2017-04-26 17:25:34 +0200

    client it'sok

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 import requests
+import argparse
+import datetime
 
-data = {
-    'title': 'Nuova Golf',
-    'data': '27 02 2017',
-    'post': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-}
-r = requests.post('http://127.0.0.1:5000', data)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--title', dest='title')
+	parser.add_argument('--article', dest='article')
+	args = parser.parse_args()
+
+	title = args.title
+	date = datetime.datetime.today()
+	date = '{}-{}-{}'.format(date.day, date.month, date.year)
+	article = args.article
+	
+	data = {
+		'title': title,
+		'data': date,
+		'post': article
+	}
+	
+	r = requests.post('http://127.0.0.1:5000/posts', data)
