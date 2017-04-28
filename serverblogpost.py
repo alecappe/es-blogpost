47eb00cdb2718986209668a46e24783022744e4e
Alessandro Cappellini <cappe9275@hotmail.it>
2017-04-28 15:49:25 +0200

    Server: trasformation from sqlite in peewee

--- serverblogpost.py
+++ serverblogpost.py
@@ -1,37 +1,63 @@
 #!/usr/bin/env python
-import sqlite3
-from flask import Flask, render_template, request
-from flask_restful import Api, Resource
+from peewee import *
+from flask import Flask
+from flask import render_template
+from flask import request
+from http.client import CREATED
+from http.client import NO_CONTENT
+from http.client import NOT_FOUND
+from http.client import OK
+from flask_restful import Api
+from flask_restful import Resource
+from flask_restful import reqparse
+
 
 app = Flask(__name__)
 api = Api(app)
 
-conn = sqlite3.connect('blog.db', check_same_thread=False)
-conn.row_factory = sqlite3.Row
-c = conn.cursor()
+db = SqliteDatabase('blog2.db')
+db.connect()
 
-class PostBlog(Resource):
+class Posts(Model):
+    title = TextField()
+    data = DateTimeField(default=[SQL('CURRENT_TIMESTAMP')])
+    post = TextField()
+
+    class Meta:
+        database = db
+
+def non_empty_str(val, name):
+    if not str(val).strip():
+        raise ValueError('The argument {} is not empty'.format(name))
+    return str(val)
+
+class Blogpost(Resource):
+    def get(self):
+        post_list = []
+        db_blog = Posts.select()
+        for post in db_blog:
+            post_list.append({'title': post.title, 'post': post.post})
+        return post_list, OK
+
     def post(self):
-        params = [
-            request.form['title'],
-            request.form['data'],
-            request.form['post']
-        ]
-        sql = ''' insert into posts(title, data, post)
-                values(?,?,?)'''
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=non_empty_str, required=True)
+        parser.add_argument('post', type=non_empty_str, required=True)
+        args = parser.parse_args(strict=True)
 
-        c.execute(sql,params)
-        conn.commit()
-        return '', 201
+        Posts.create(title=args['title'], post=args['post'])
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    sql = 'SELECT * FROM posts'
-    ctx = {'posts': c.execute(sql)}
+        return '', CREATED
+
+@app.route('/')
+def get():
+    resp_get = Posts.select()
+    ctx = {'posts': resp_get}
 
     return render_template('index.html', **ctx)
 
-api.add_resource(PostBlog, '/posts')
+api.add_resource(Blogpost, '/posts')
 
 if __name__ == '__main__':
+    Posts.create_table(fail_silently=True)
     app.run(debug=True)
