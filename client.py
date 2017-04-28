368b11e294cb928e9f485d6a2a7857e4532731de
Alessandro Cappellini <cappe9275@hotmail.it>
2017-04-28 17:11:25 +0200

    client not commit datetime but db create datetime and add required argument obligation

--- client.py
+++ client.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 import requests
 import argparse
-import datetime
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--title', dest='title')
-	parser.add_argument('--article', dest='article')
+	parser.add_argument('--title', dest='title', required=True)
+	parser.add_argument('--article', dest='article', required=True)
 	args = parser.parse_args()
 
 	title = args.title
-	date = datetime.datetime.today()
-	date = '{}-{}-{}'.format(date.day, date.month, date.year)
+	
 	article = args.article
 	
 	data = {
 		'title': title,
-		'data': date,
 		'post': article
 	}
 	
-	r = requests.post('http://127.0.0.1:5000/posts', data)
+	r = requests.post('http://127.0.0.1:5000/posts', data=data)
